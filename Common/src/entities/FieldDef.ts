e9839e87370e74ca656bf42077b0ca23594c461f
CFM017 <CFM017@ESL000254>
2016-05-17 09:52:48 +0200

    project commit

---
+++ Common/src/entities/FieldDef.ts
@@ -0,0 +1,17 @@
+﻿module app.infrastructure.entities {
+
+    export interface IFieldDef {
+        Label: string,
+        Field: string,
+        Datatype: string
+    }
+
+    export class FieldDef implements IFieldDef {
+        constructor(
+            public Label: string,
+            public Field: string,
+            public Datatype: string
+        ) {
+        }
+    }
+}
